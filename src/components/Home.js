6b4e684e8aaf19a9479dec5a35b77872962bd0ef
eric santos <eric355402@yahoo.com>
2021-07-19 12:25:22 -0600

    added webm video, testing on mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "../styles/home.css";
-import bkgvideo from "../bkg-videos/zombies.mp4";
+import bkgvideo from "../bkg-videos/zombies.webm";
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <video autoPlay muted loop id="myVideo">
         <source src={bkgvideo} type="video/mp4" />
       </video>
       <header className="viewport-header">
         <h1>Ep Zombie Fest</h1>
       </header>
-    </>
+    </div>
   );
 };
 export default Home;
